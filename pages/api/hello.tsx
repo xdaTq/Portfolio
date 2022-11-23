785cfddd4b9330fe08630c5b67b20af95e02b6f5
xdaTq <kujawski.erwin@gmail.com>
2022-11-23 23:57:17 +0100

    fix/console erros popup

--- pages/api/hello.tsx
+++ pages/api/hello.tsx
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 export {}
 /*
+test
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
 }
