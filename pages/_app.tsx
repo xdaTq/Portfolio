c642430972122efed8aa165fadff42443f7eda3d
aTq_ <kujawski.erwin@gmail.com>
2022-11-24 07:34:35 +0100

    fix/html fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,13 @@
 import '../styles/globals.css'
 import { DefaultSeo } from 'next-seo'
 import seo from '../next-seo.config'
-import { Html } from 'next/document'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Html lang="en">
+    <>
       <DefaultSeo {...seo} />
       <Component {...pageProps} />
-    </Html>
+    </>
   )
 }
 
